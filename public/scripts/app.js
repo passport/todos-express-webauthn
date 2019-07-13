5923c5f6f6cc4ae0bbf739f7b43c30cbfedc6d3f
Jared Hanson <jaredhanson@gmail.com>
2019-07-12 18:13:46 -0700

    POST credential

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,40 @@
+function _arrayBufferToBase64( buffer ) {
+    var binary = '';
+    var bytes = new Uint8Array( buffer );
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode( bytes[ i ] );
+    }
+    return window.btoa( binary );
+}
+
+function publicKeyCredentialToJSON(cred) {
+  if (cred instanceof Array) {
+      var arr = [];
+      for(var i of cred)
+          arr.push(publicKeyCredentialToJSON(i));
+
+      return arr
+  }
+
+  if (cred instanceof ArrayBuffer) {
+      return _arrayBufferToBase64(cred)
+  }
+
+  if (cred instanceof Object) {
+      let obj = {};
+
+      for (var key in cred) {
+          obj[key] = publicKeyCredentialToJSON(cred[key])
+      }
+
+      return obj
+  }
+
+  return cred
+}
+
+
 window.onload = function() {
   console.log('loaded...');
   
@@ -24,6 +61,28 @@ window.onload = function() {
         json.user.id = enc.encode(json.user.id); // encode to ArrayBuffer
         
         navigator.credentials.create({ publicKey: json })
+          .then(function(response) {
+            console.log(response);
+            
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', '/account/credential', true);
+            xhr.onreadystatechange = function() {
+              console.log('REGISTER READY STATE CHANGE!')
+              console.log(this.readyState);
+              console.log(this.status);
+              console.log(this.responseText)
+            };
+            
+            
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.send(JSON.stringify(publicKeyCredentialToJSON(response)));
+          })
+          .catch(function(err) {
+            console.log('ERROR');
+            console.log(err);
+            console.log(err.code);
+            console.log(err.message);
+          })
         
       }
     };
