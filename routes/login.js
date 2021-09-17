515795e8a638693facefb6b1ea425c2edb6c2b0b
Jared Hanson <jaredhanson@gmail.com>
2021-09-16 17:26:33 -0700

    Lookup keys for challenge with username.

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var passport = require('passport');
 var base64url = require('base64url');
+var db = require('../db');
+
 var router = express.Router();
 
 router.get('/login',
@@ -14,44 +16,67 @@ router.post('/login',
     console.log(req.headers);
     console.log(req.body)
     
-    res.format({
-      'application/json': function () {
-        var options = {
-          challenge: '5678', // TODO: Make this random,
-    
-          /*
-          allowCredentials: [ {
-            type: 'public-key',
-            //id: 'p5XN9H3kPWVymj0GcGDZtElg2Y2g4AISMWyUjjz3HEZBeHjamcknUsyTDy15zEGRXJCsyFVAEQ8Aq8_PbdkZnw',
-            //id: 'jWcLBKE7CnLmJTecFZNTSFnWyaokRzpu3cg-b1qMBweUpCHMPhZ9MVL4oVFI3Vb4_K-d4wJoxfjjZ4uU17xNQA',
-            //id: 'Vj1ZA6yKxblqKMhVduxJdOUt9tdz6333z7vygjM6qj8ZMc07m3tyGHwGYRDBOPl1PGRqHxdRQraYbaTjC7p0NQ',
-            //id: 'JYrR3EvvQJNqG0i_OwJckOkbzq4YJWviotG4hig9wA_Qdxm-eBEHfsYqBJKTtXMasL-RD9CFOlcag48icK3E8Q',
-            //id: 'n90ZI-FwPA9HT5jtZins33Rtae1Zz1zLnoDR9yCj5Jwz2PB6fJR0KCPZehORB-ht48mRfbcA512cnDyfbQQ0OQ'
-            //id: 'i18s3M25qA39Y6vOXR2_TOCglKz8kxFHHzx6Jpnk_Y9THMVBV85Vnd5IyjtNpFIS6Sp_ssg4ZJtAW6UARMStUQ',
-            //id: 'AIJBqkpwDr_4baNTt2_u_kG-sGqZnr4WZ63y911uY9qB6u6JTcB-9MQkyQzruTOBRi9vKluqAZqBWio2tFem-SgrUD7RI7i_Bpajs5N6uG_cCdycJwE-4Xjt',
-            //id: 'Abqu4O_U5dE71w4TuJ-zW1IrpdCgZftpnR-hKqfTWheMc8SZIaky7qXAyiDzPSqRtPUC',
-            //id: 'AcZZ65l8zcISWWFOdY5Era6WnZPK5E-CBvo-qPlZas7KbvyKOj_LTBMuJkwsyqs8P38J',
-            //id: 'AcQm4Bg7BONKr6OWOuCkMRUF8Z7UGkkEfqkEeoy1DmLW7Lp1f7zxYxpjI-2wtV9FlRiV',
-            //id: 'AR3tgY3DAncMTt179Kcg-G_FdWu1E2eOqKXB_OtwaTJ-YITN8vvCST1qIWpirqQ1t93q'
-            //id: 'GU0lmsssQL3nKuu3Q5YtBTVfTLUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-            //id: '123',
-            //id: 'ARc4h0RwdGkBHuvD4XeYVRPCMh0U1GIZ7ByW39DUd2bxy5ue55OftYqzivcfrcABMXtr',
-            //id: 'AcjZv01ljIwB4uxZrKUEEXTttjg2NEy2QkkvrF6QlWdatZ2CTBBVzXB6PHg1FR6KbMDr'
-            //id: 'AbmCd_HL3HoNUM4OnQoxHbzx7n6RqJ6m1FrRqcuN2cx8B76wEUd-r3hITX2l8RUnfszD',
-            id: 'ARRq0HnxomOUCSBwTuVa9PEIp7sEYQ7IdBaIzeEFFDgFQ0VzoDHlCcqYZSeEMDvbDA0N',
-            //transports: ['usb', 'nfc', 'ble']
-            transports: ['internal']
-          } ]*/
-        };
+    function challenge(keys) {
+      res.format({
+        'application/json': function () {
+          var options = {
+            challenge: '5678', // TODO: Make this random,
+          };
+          
+          if (keys) {
+            options.allowCredentials = [];
+            
+            keys.forEach(function(row, i) {
+              var key = {
+                type: 'public-key',
+                id: row.external_id
+                // TODO: put transports in
+                //transports: ['usb', 'nfc', 'ble']
+                //transports: ['internal']
+              };
         
-        res.send(options);
-      },
+              options.allowCredentials.push(key);
+            });
+          }
+          
+          console.log(options)
+          
+          res.send(options);
+        },
 
-      default: function () {
-        // TODO
-      }
-    });
+        default: function () {
+          // TODO
+        }
+      });
+    }
     
+    
+    var username = req.body.username;
+    if (!username) {
+      // challenge for client-side discoverable keys (aka resident keys)
+      challenge();
+    } else {
+      console.log('TODO: lookupu username');
+      
+      db.get('SELECT rowid AS id FROM users WHERE username = ?', [ username ], function(err, row) {
+        if (err) { return cb(err); }
+        
+        // TODO: Handle undefined row
+        //if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+        
+        console.log('FOUND USER');
+        console.log(row);
+        
+        db.all('SELECT rowid as id, * FROM public_key_credentials WHERE user_id = ?', [ row.id ], function(err, rows) {
+          if (err) { return next(err); }
+          
+          console.log('GOT KEYS');
+          console.log(rows);
+          
+          challenge(rows);
+        });
+      });
+    }
     
   });
 
