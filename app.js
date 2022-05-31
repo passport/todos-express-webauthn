e0292794090fd37124eb896f01c0dfac63dcf146
Jared Hanson <jaredhanson@gmail.com>
2022-05-31 11:18:03 -0700

    Update app and db.

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
+var createError = require('http-errors');
 var express = require('express');
-var passport = require('passport');
 var path = require('path');
-//var cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var csrf = require('csurf');
+var passport = require('passport');
 var logger = require('morgan');
 
+// pass the session to the connect sqlite3 module
+// allowing it to inherit from session.Store
+var SQLiteStore = require('connect-sqlite3')(session);
+
 var indexRouter = require('./routes/index');
-var authRouter = require('./routes/login');
-var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
 
 var app = express();
-
-require('./boot/db')();
-require('./boot/auth')();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Use application-level middleware for common functionality, including
-// logging, parsing, and session handling.
+app.locals.pluralize = require('pluralize');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something stored
+  store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
+}));
+app.use(csrf());
+app.use(passport.authenticate('session'));
 app.use(function(req, res, next) {
   var msgs = req.session.messages || [];
   res.locals.messages = msgs;
@@ -32,11 +42,28 @@ app.use(function(req, res, next) {
   req.session.messages = [];
   next();
 });
-app.use(passport.initialize());
-app.use(passport.authenticate('session'));
+app.use(function(req, res, next) {
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/', authRouter);
-app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
--- db.js
+++ db.js
@@ -1,3 +1,32 @@
 var sqlite3 = require('sqlite3');
+var mkdirp = require('mkdirp');
 
-module.exports = new sqlite3.Database('db.sqlite3');
+mkdirp.sync('./var/db');
+
+var db = new sqlite3.Database('./var/db/todos.db');
+
+db.serialize(function() {
+  db.run("CREATE TABLE IF NOT EXISTS users ( \
+    id INTEGER PRIMARY KEY, \
+    username TEXT UNIQUE, \
+    hashed_password BLOB, \
+    salt BLOB, \
+    name TEXT \
+  )");
+  
+  db.run("CREATE TABLE IF NOT EXISTS public_key_credentials ( \
+    id INTEGER PRIMARY KEY, \
+    user_id INTEGER NOT NULL, \
+    external_id TEXT UNIQUE, \
+    public_key TEXT \
+  )");
+  
+  db.run("CREATE TABLE IF NOT EXISTS todos ( \
+    id INTEGER PRIMARY KEY, \
+    owner_id INTEGER NOT NULL, \
+    title TEXT NOT NULL, \
+    completed INTEGER \
+  )");
+});
+
+module.exports = db;
