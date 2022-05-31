8f11b2f0c6a1601b32398f292a9bd58dd091ab18
Jared Hanson <jaredhanson@gmail.com>
2022-05-31 11:58:06 -0700

    Register events only if PublicKeyCredential is supported.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -57,70 +57,70 @@ function urlencodedFormData(data){
 }
 
 
-window.onload = function() {
-  console.log('LOAD!');
+window.addEventListener('load', function() {
   
-  //return;
+  if (window.PublicKeyCredential) {
   
-  document.getElementById('register').addEventListener('click', function(e) {
-    if (!window.PublicKeyCredential) { return; } // submit the form normally
+    document.querySelector('form').addEventListener('submit', function(event) {
+      event.preventDefault();
+    
+      console.log('sign up!');
+      return;
     
     
-    console.log('sign up...');
-    e.preventDefault();
-    
-    
-    
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
       
-      if (this.readyState === XMLHttpRequest.DONE) {
-        console.log(this.responseText)
-        //return;
+        if (this.readyState === XMLHttpRequest.DONE) {
+          console.log(this.responseText)
+          //return;
         
-        var json = JSON.parse(this.responseText);
+          var json = JSON.parse(this.responseText);
         
-        var userID = json.user.id;
+          var userID = json.user.id;
         
-        var enc = new TextEncoder(); // always utf-8
-        json.challenge = enc.encode(json.challenge); // encode to ArrayBuffer
-        json.user.id = enc.encode(json.user.id); // encode to ArrayBuffer
+          var enc = new TextEncoder(); // always utf-8
+          json.challenge = enc.encode(json.challenge); // encode to ArrayBuffer
+          json.user.id = enc.encode(json.user.id); // encode to ArrayBuffer
         
-        console.log('CREATE WITH');
-        console.log(json)
+          console.log('CREATE WITH');
+          console.log(json)
         
-        navigator.credentials.create({ publicKey: json })
-          .then(function(response) {
-            console.log(response);
+          navigator.credentials.create({ publicKey: json })
+            .then(function(response) {
+              console.log(response);
             
-            var xhr = new XMLHttpRequest();
-            xhr.open('POST', '/login/public-key/2', true);
-            xhr.onreadystatechange = function() {
-              console.log('REGISTER READY STATE CHANGE!')
-              console.log(this.readyState);
-              console.log(this.status);
-              console.log(this.responseText)
-            };
+              var xhr = new XMLHttpRequest();
+              xhr.open('POST', '/login/public-key/2', true);
+              xhr.onreadystatechange = function() {
+                console.log('REGISTER READY STATE CHANGE!')
+                console.log(this.readyState);
+                console.log(this.status);
+                console.log(this.responseText)
+              };
             
-            xhr.setRequestHeader('Content-Type', 'application/json');
-            // TODO: Remove this in favor of session
-            //xhr.setRequestHeader('X-User-ID', userID);
-            xhr.send(JSON.stringify(publicKeyCredentialToJSON(response)));
-          })
-          .catch(function(err) {
-            console.log('ERROR');
-            console.log(err);
-            console.log(err.code);
-            console.log(err.message);
-          });
-      }
-    };
+              xhr.setRequestHeader('Content-Type', 'application/json');
+              // TODO: Remove this in favor of session
+              //xhr.setRequestHeader('X-User-ID', userID);
+              xhr.send(JSON.stringify(publicKeyCredentialToJSON(response)));
+            })
+            .catch(function(err) {
+              console.log('ERROR');
+              console.log(err);
+              console.log(err.code);
+              console.log(err.message);
+            });
+        }
+      };
     
-    var formEl = document.querySelector('form');
-    var formData = new FormData(formEl);
-    xhr.open('POST', formEl.action, true);
-    xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-    xhr.setRequestHeader('Accept', 'application/json');
-    xhr.send(urlencodedFormData(formData));
-  });
-};
+      var formEl = document.querySelector('form');
+      var formData = new FormData(formEl);
+      xhr.open('POST', formEl.action, true);
+      xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+      xhr.setRequestHeader('Accept', 'application/json');
+      xhr.send(urlencodedFormData(formData));
+    });
+  
+  }
+  
+});
