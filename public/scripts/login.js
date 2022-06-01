8473fe9f83f1effba5b4a16a03c56af2b0c5755b
Jared Hanson <jaredhanson@gmail.com>
2022-06-01 07:39:23 -0700

    Remove use of XHR from login in favor of fetch.

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -62,10 +62,7 @@ window.addEventListener('load', function() {
   document.querySelector('form').addEventListener('submit', function(event) {
     if (!window.PublicKeyCredential) { return; }
     
-    
     event.preventDefault();
-    
-    console.log('login...');
     
     var encoder = new TextEncoder();
     
@@ -79,7 +76,6 @@ window.addEventListener('load', function() {
       }
     })
     .then(function(credential) {
-      console.log('got');
       console.log(credential);
       
       return fetch('/login/public-key', {
@@ -100,68 +96,6 @@ window.addEventListener('load', function() {
     .catch(function(error) {
       console.log(error);
     });
-    
-    
-    return;
-    
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-    
-      if (this.readyState === XMLHttpRequest.DONE) {
-        console.log(this.responseText)
-        
-        
-        var json = JSON.parse(this.responseText);
-        
-        var enc = new TextEncoder(); // always utf-8
-        json.challenge = enc.encode(json.challenge); // encode to ArrayBuffer
-        if (json.allowCredentials) {
-        //json.allowCredentials[0].id = enc.encode(json.allowCredentials[0].id); // encode to ArrayBuffer
-        //json.allowCredentials[0].id = base64url.decode(json.allowCredentials[0].id);
-        
-          var i = 0, len = json.allowCredentials.length;
-          for (i = 0; i < len; ++i) {
-            json.allowCredentials[i].id = base64url.decode(json.allowCredentials[i].id);
-          }
-        
-        }
-      
-        console.log(json);
-        //return;
-        
-        navigator.credentials.get({ publicKey: json })
-          .then(function(response) {
-            console.log(response)
-            //return;
-          
-            var xhr = new XMLHttpRequest();
-            xhr.open('POST', '/login/public-key', true);
-            xhr.onreadystatechange = function() {
-              console.log(this.readyState);
-              console.log(this.status);
-              console.log(this.responseText)
-              
-              if (this.readyState === XMLHttpRequest.DONE) {
-                window.location = '/';
-              }
-            };
-            
-            xhr.setRequestHeader('Content-Type', 'application/json');
-            xhr.send(JSON.stringify(publicKeyCredentialToJSON(response)));
-          })
-          .catch(function(err) {
-            console.log(err);
-            console.log(err.code);
-            console.log(err.message);
-          });
-      }
-    };
-    
-    var formEl = document.querySelector('form');
-    var formData = new FormData(formEl);
-    xhr.open('POST', formEl.action, true);
-    xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-    xhr.setRequestHeader('Accept', 'application/json');
-    xhr.send(urlencodedFormData(formData));
   });
+  
 });
