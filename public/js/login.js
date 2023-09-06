16796ff18538599340fc46d1b0fc520b03610eda
Jared Hanson <jaredhanson@gmail.com>
2023-09-06 14:30:50 -0700

    Add conditional mediation to login form.

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,10 @@
 window.addEventListener('load', function() {
   
   document.getElementById('siw-public-key').addEventListener('click', function(event) {
-    if (!window.PublicKeyCredential) { return; }
+    if (!window.PublicKeyCredential) {
+      alert('Passkeys are not supported by this browser');
+      return;
+    }
     
     event.preventDefault();
     
@@ -17,11 +20,7 @@ window.addEventListener('load', function() {
     .then(function(json) {
       return navigator.credentials.get({
         publicKey: {
-          challenge: base64url.decode(json.challenge),
-          //allowCredentials: [
-          //  { type: 'public-key', id: base64url.decode('VjXl8fuJXIAqLg-BVrR5oeLLfee6gBGKXdMxo6xtMySugJfU2HNvTJk84T1DgFYtJDpDrwL2Bg_QM4xQwVAutA') },
-          //  { type: 'public-key', id: base64url.decode('noMuGuaaVLubAVjuS6Z2BYrrBpajYhtjnFgvSjk0IV1LJeVrupbpnw') }
-          //]
+          challenge: base64url.decode(json.challenge)
         }
       });
     })
@@ -58,5 +57,64 @@ window.addEventListener('load', function() {
       console.log(error);
     });
   });
+  
+  if (window.PublicKeyCredential && PublicKeyCredential.isConditionalMediationAvailable) {
+    PublicKeyCredential.isConditionalMediationAvailable()
+    .then(function(available) {
+      if (!available) { return; }
+      
+      document.getElementById('siw-public-key').remove();
+      
+      return fetch('/login/public-key/challenge', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json'
+        }
+      })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        return navigator.credentials.get({
+          mediation: 'conditional',
+          publicKey: {
+            challenge: base64url.decode(json.challenge)
+          }
+        });
+      })
+      .then(function(credential) {
+        var body = {
+          id: credential.id,
+          response: {
+            clientDataJSON: base64url.encode(credential.response.clientDataJSON),
+            authenticatorData: base64url.encode(credential.response.authenticatorData),
+            signature: base64url.encode(credential.response.signature),
+            userHandle: credential.response.userHandle ? base64url.encode(credential.response.userHandle) : null
+          }
+        };
+        if (credential.authenticatorAttachment) {
+          body.authenticatorAttachment = credential.authenticatorAttachment;
+        }
+    
+        return fetch('/login/public-key', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: JSON.stringify(body)
+        });
+      })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        window.location.href = json.location;
+      });
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+  }
   
 });
