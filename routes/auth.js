f21e0485971c131c71aba9dba5886dff269e7eb0
Jared Hanson <jaredhanson@gmail.com>
2023-09-07 15:42:24 -0700

    Support registering keys to existing accounts.

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ var express = require('express');
 var passport = require('passport');
 var WebAuthnStrategy = require('passport-fido2-webauthn');
 var SessionChallengeStore = require('passport-fido2-webauthn').SessionChallengeStore;
+var ensureLogIn = require('connect-ensure-login').ensureLoggedIn;
 var base64url = require('base64url');
 var uuid = require('uuid').v4;
 var db = require('../db');
@@ -14,35 +15,52 @@ passport.use(new WebAuthnStrategy({ store: store }, function verify(id, userHand
     if (err) { return cb(err); }
     if (!row) { return cb(null, false, { message: 'Invalid key. '}); }
     var publicKey = row.public_key;
-    db.get('SELECT * FROM users WHERE rowid = ?', [ row.user_id ], function(err, row) {
+    db.get('SELECT * FROM users WHERE id = ?', [ row.user_id ], function(err, row) {
       if (err) { return cb(err); }
       if (!row) { return cb(null, false, { message: 'Invalid key. '}); }
-      if (Buffer.compare(row.handle, userHandle) != 0) {
+      // TODO: how should userHandle be checked?
+      //if (Buffer.compare(row.handle, userHandle) != 0) {
+      if (userHandle && Buffer.compare(row.handle, userHandle) != 0) {
         return cb(null, false, { message: 'Invalid key. '});
       }
       return cb(null, row, publicKey);
     });
   });
 }, function register(user, id, publicKey, cb) {
-  db.run('INSERT INTO users (username, name, handle) VALUES (?, ?, ?)', [
-    user.name,
-    user.displayName,
-    user.id
-  ], function(err) {
+  db.get('SELECT * FROM users WHERE handle = ?', [ user.id ], function(err, localUser) {
     if (err) { return cb(err); }
-    var newUser = {
-      id: this.lastID,
-      username: user.name,
-      name: user.displayName
-    };
-    db.run('INSERT INTO public_key_credentials (user_id, external_id, public_key) VALUES (?, ?, ?)', [
-      newUser.id,
-      id,
-      publicKey
-    ], function(err) {
-      if (err) { return cb(err); }
-      return cb(null, newUser);
-    });
+    
+    if (!localUser) {
+      db.run('INSERT INTO users (username, name, handle) VALUES (?, ?, ?)', [
+        user.name,
+        user.displayName,
+        user.id
+      ], function(err) {
+        if (err) { return cb(err); }
+        var newUser = {
+          id: this.lastID,
+          username: user.name,
+          name: user.displayName
+        };
+        db.run('INSERT INTO public_key_credentials (user_id, external_id, public_key) VALUES (?, ?, ?)', [
+          newUser.id,
+          id,
+          publicKey
+        ], function(err) {
+          if (err) { return cb(err); }
+          return cb(null, newUser);
+        });
+      });
+    } else {
+      db.run('INSERT INTO public_key_credentials (user_id, external_id, public_key) VALUES (?, ?, ?)', [
+        localUser.id,
+        id,
+        publicKey
+      ], function(err) {
+        if (err) { return cb(err); }
+        return cb(null, localUser);
+      });
+    }
   });
 }));
 
@@ -59,13 +77,20 @@ passport.deserializeUser(function(user, cb) {
 });
 
 
+var ensureLoggedIn = ensureLogIn();
+
 var router = express.Router();
 
 router.get('/login', function(req, res, next) {
   res.render('login');
 });
 
-router.post('/login/public-key', passport.authenticate('webauthn', {
+function dumpBody(req, res, next) {
+  console.log(req.body);
+  next();
+}
+
+router.post('/login/public-key', dumpBody, passport.authenticate('webauthn', {
   failureMessage: true,
   failWithError: true
 }), function(req, res, next) {
@@ -101,6 +126,27 @@ router.post('/signup/public-key/challenge', function(req, res, next) {
     id: handle,
     name: req.body.username,
     displayName: req.body.name
+  };
+  store.challenge(req, { user: user }, function(err, challenge) {
+    if (err) { return next(err); }
+    user.id = base64url.encode(user.id);
+    res.json({ user: user, challenge: base64url.encode(challenge) });
+  });
+});
+
+// TODO: ensureLoggedIn shouldn't be used here, since its an API-like request, not redirect
+router.post('/account/public-key/challenge', ensureLoggedIn, function(req, res, next) {
+  db.get('SELECT * FROM users WHERE id = ?', [ req.user.id ], function(err, user) {
+    if (err) { return next(err); }
+    // TODO: handle no row
+    res.locals.handle = user.handle;
+    return next();
+  });
+}, function(req, res, next) {
+  var user = {
+    id: res.locals.handle,
+    name: req.user.username,
+    displayName: req.user.name
   };
   store.challenge(req, { user: user }, function(err, challenge) {
     if (err) { return next(err); }
